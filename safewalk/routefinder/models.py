87c4e1648bb81e1e0fef8fc950709e8032ce3549
Unknown <anjale98@gmail.com>
2018-03-24 21:00:28 -0400

    Added Model

--- safewalk/routefinder/models.py
+++ safewalk/routefinder/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Crime(models.Model):
+    latitude = models.FloatField(max_length=10)
+    longitude = models.FloatField(max_length=10)
+    date = models.DateTimeField('date committed')
+    
--- safewalk/routefinder/views.py
+++ safewalk/routefinder/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world. You are at the RouteFinder Homepage.")
+
+def crime(request, crime_id):
+    return HttpResponse("Crime committed at ")
